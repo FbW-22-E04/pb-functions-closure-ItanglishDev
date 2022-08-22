d73bd8beb2ad99b965223e8edac17dbc7ed3d901
chris <chrishethertonweb@gmail.com>
2022-08-22 14:37:10 +0200

    2a answered

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Closure is the process of ending a situation, a "process". In javascript it allows you to reiterate through a function that is another function
 
+
+/**2 a
+ * The way we are returning the anonymous function makes me think that there is a closure
+ */
